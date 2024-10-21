84562238020fbe6639681242e2e8fd7fa144dffb
Delvin <143737853+bancakes@users.noreply.github.com>
2024-10-21 17:23:32 +0000

    h

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -23,10 +23,11 @@ from django.conf.urls.static import static
 from hello_world.core import views as core_views
 
 urlpatterns = [
+    path("", include('dolst.urls'))
     path("", core_views.index),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-]
+    ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
